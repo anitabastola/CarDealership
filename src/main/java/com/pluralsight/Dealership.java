a8d5c749f1ffcf8bcc923b8c92faaecc3ee85027
student <abastola@pgh.yearup.org>
2024-05-13 17:21:48 -0400

    added the getters and setters for every data

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,53 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void addVehicle() {
+
+    }
+
+    public void removeVehicle() {
+
+    }
+}
---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    private Dealership dealership;
+    private Scanner scanner;
+
+    public UserInterface() {
+        scanner = new Scanner(System.in);
+    }
+    public void display() {
+
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetbyYearRequest() {
+
+    }
+
+    public void processGetbyColorRequest() {
+
+    }
+    public void processGetbyMileageRequest() {
+
+    }
+    public void processGetbyVehicleRequest() {
+
+    }
+    public void processGetAllVehicleRequest() {
+
+    }
+    public void processAddVehicleRequest() {
+
+    }
+    public void processRemoveVehicleRequest() {
+
+    }
+}
