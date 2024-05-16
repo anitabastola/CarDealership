e134ed897f2e74651e2b5e83aa11962a668fe3c3
student <abastola@pgh.yearup.org>
2024-05-15 23:30:59 -0400

    display method

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,5 +1,7 @@
 package com.pluralsight;
 
+import java.util.List;
+import java.util.PrimitiveIterator;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -9,24 +11,71 @@ public class UserInterface {
     public UserInterface(Dealership dealership, Scanner scanner) {
         this.dealership = dealership;
         this.scanner = scanner;
-       // Scanner scanner = new Scanner(System.in);
     }
+
     private void init() {
         DealershipFileManager fileManager = new DealershipFileManager();
         this.dealership = fileManager.getDealership();
     }
-    public void display() {
+
+        public void displayVehicle(List <Vehicle> inventory) {
+            for (Vehicle vehicle : inventory) {
+                System.out.println(vehicle);
+            }
+
         boolean running = true;
+
         while (running) {
 
-            System.out.println("Welcome to TransactionApp");
-            System.out.println("Choose an option:");
-            System.out.println("D) Add Deposit");
-            System.out.println("P) Make Payment (Debit)");
-            System.out.println("L) Ledger");
-            System.out.println("X) Exit");
+            System.out.println("Welcome to our Car Dealership ");
+            System.out.println("Please Choose an option: ");
+            System.out.println("1) Look up vehicles by price ");
+            System.out.println("2) Look up vehicles by make/model ");
+            System.out.println("3) Look up vehicles by year ");
+            System.out.println("4) Look up vehicles by color ");
+            System.out.println("5) Look up vehicles by mileage");
+            System.out.println("6) Look up vehicles by type");
+            System.out.println("7) Look up ALL vehicles  ");
+            System.out.println("8) Add vehicle ");
+            System.out.println("9) Remove vehicle ");
+            System.out.println("0) Exit");
 
             String input = scanner.nextLine().trim();
+
+            switch (input.toUpperCase()) {
+                case "1":
+                    processGetByPriceRequest();
+                    break;
+                case "2":
+                    processGetByMakeModelRequest();
+                    break;
+                case "3":
+                    processGetbyYearRequest();
+                    break;
+                case "4":
+                    processGetbyColorRequest();
+                    break;
+                case "5":
+                    processGetbyMileageRequest();
+                    break;
+                case "6":
+                    processGetbyVehicleRequest();
+                    break;
+                case "7":
+                    processGetAllVehicleRequest();
+                    break;
+                case "8":
+                    processAddVehicleRequest();
+                    break;
+                case "9":
+                    processRemoveVehicleRequest();
+                    break;
+                default:
+                    System.out.println("Invalid, try again");
+                    break;
+
+            }
+
 
                 }
 
@@ -34,10 +83,10 @@ public class UserInterface {
 
 
 
-
-
-    public void processGetByPriceRequest() {
+        public void processGetByPriceRequest() {
         System.out.println("Please enter your minimum price ");
+        Scanner scanner = new Scanner(System.in);
+
 
     }
 
@@ -59,7 +108,8 @@ public class UserInterface {
 
     }
     public void processGetAllVehicleRequest() {
-
+        List<Vehicle> inventory = dealership.getAllVehicles();
+        displayVehicle(inventory);
     }
     public void processAddVehicleRequest() {
 
